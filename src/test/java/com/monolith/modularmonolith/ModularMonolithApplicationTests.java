450868744f677111343c36ecd914e5c85c107b2e
Gevicodeur <ndilbedouvic@gmail.com>
2026-06-19 11:13:55 +0100

    Installation Projet et configuration

---
+++ src/test/java/com/monolith/modularmonolith/ModularMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monolith.modularmonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModularMonolithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
